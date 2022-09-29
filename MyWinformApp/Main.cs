9be4eebbb9c92c0d88eb7a211936ec6b4de7e102
Digit24-dev <57099725+Digit24-dev@users.noreply.github.com>
2022-09-29 23:20:21 +0900

    [Client] - UserList update fixed. (excepting null data.)

--- MyWinformApp/Main.cs
+++ MyWinformApp/Main.cs
@@ -137,6 +137,8 @@ namespace MyWinformApp
                     UsersList.Items.Clear();
                     foreach (string item in userList)
                     {
+                        if (item.Equals(""))
+                            break;
                         UsersList.Items.Add(item);
                     }
                 }));
